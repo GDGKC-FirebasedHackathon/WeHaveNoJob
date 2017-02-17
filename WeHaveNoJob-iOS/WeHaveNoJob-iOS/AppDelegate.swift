6b88b80c418b6d4b65d3e1cfc2d91d7bc4fc4730
minjaeJo <alswo2722@naver.com>
2017-02-17 19:56:09 +0900

    restart_ios

--- WeHaveNoJob-iOS/WeHaveNoJob-iOS/AppDelegate.swift
+++ WeHaveNoJob-iOS/WeHaveNoJob-iOS/AppDelegate.swift
@@ -7,19 +7,12 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-        private func application(application: UIApplication,
-                         didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?)
-            -> Bool {
-                FIRApp.configure()
-                return true
-        }
-    
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
